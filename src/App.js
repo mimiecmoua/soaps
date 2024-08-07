78b7b2934cb2190b526e275b7bafbc0dd6244cf3
Emilie <dionatsumi@gmail.com>
2024-08-07 20:20:48 +0200

    New App.js

--- src/App.js
+++ src/App.js
@@ -4,35 +4,38 @@ import Home from './components/Home';
 import Products from './components/Products';
 import Product from './components/Product';
 import About from './components/About';
-import Contact from './components/Contact'; 
+import Contact from './components/Contact';
 import Cart from './components/Cart';
 import Register from './components/Register';
-import { Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Checkout from './components/Checkout';
 import Footer from './components/Footer';
+import { Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
-    <>
-       
+    <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:id" element={<Product />} />
-        <Route path="/cart" element={<Cart />} /> 
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/checkout" element={<Checkout />} />
-      </Routes>
+      <div className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
 
 export default App;
+
 
 
